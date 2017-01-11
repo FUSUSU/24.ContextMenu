aef857fc64c5ee536c65470b629b4e418c385690
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-11 22:21:55 +0700

    Ánh xạ

--- app/src/main/java/com/example/teo/a24contextmenu/MainActivity.java
+++ app/src/main/java/com/example/teo/a24contextmenu/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Ánh xạ
+        listView1 = (ListView) findViewById(R.id.listView1);
     }
 }
