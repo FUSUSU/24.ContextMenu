3fff4890acfc5b7d85f8a57bd82e5eec59d8ea68
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-11 22:20:31 +0700

    Khai báo hoàn thành

--- app/src/main/java/com/example/teo/a24contextmenu/MainActivity.java
+++ app/src/main/java/com/example/teo/a24contextmenu/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.teo.a24contextmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView1;
+    String contacts[] = {"Ajay","Sachin","Sumit","Tarun","Yogesh"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
